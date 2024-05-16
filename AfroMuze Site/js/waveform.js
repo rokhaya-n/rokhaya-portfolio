935065c991e50b20472a94eb41f8ebaa7553f9c9
rokhaya-n <144301039+rokhaya-n@users.noreply.github.com>
2024-05-16 15:49:42 -0400

    fix waveform

--- AfroMuze Site/js/waveform.js
+++ AfroMuze Site/js/waveform.js
@@ -5,40 +5,51 @@ const width = canvas.width;
 const height = canvas.height;
 
 //preloading
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-const audioElement = new Audio('music/sagacite.mp3');
-const audioSource = audioContext.createMediaElementSource(audioElement);
-const analyser = audioContext.createAnalyser();
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
+let audioContext;
+let audioElement = new Audio('music/sagacite.mp3');
+let audioSource;
+let analyser;
+let bufferLength;
+let dataArray;
+let isPlaying = false;
 
-analyser.fftSize = 2048;
-audioSource.connect(analyser);
-audioSource.connect(audioContext.destination);
+function initAudio() {
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  audioSource = audioContext.createMediaElementSource(audioElement);
+  analyser = audioContext.createAnalyser();
+  bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
+
+  analyser.fftSize = 2048;
+  audioSource.connect(analyser);
+  audioSource.connect(audioContext.destination);
+}
 
 //rendering waveform
 function renderWaveform() {
   ctx.clearRect(0, 0, width, height);
-  
+
   analyser.getByteTimeDomainData(dataArray);
   ctx.lineWidth = 2;
   ctx.strokeStyle = '#ffffff';
   ctx.beginPath();
-  
+
   const sliceWidth = width * 1.0 / bufferLength;
   let x = 0;
-  for(let i = 0; i < bufferLength; i++) {
+
+  for (let i = 0; i < bufferLength; i++) {
     const v = dataArray[i] / 128.0;
     const y = v * height / 2;
-    
-    if(i === 0) {
+
+    if (i === 0) {
       ctx.moveTo(x, y);
     } else {
       ctx.lineTo(x, y);
     }
-    
+
     x += sliceWidth;
   }
+
 
   ctx.lineTo(canvas.width, canvas.height / 2);
   ctx.stroke();
@@ -46,23 +57,32 @@ function renderWaveform() {
   requestAnimationFrame(renderWaveform);
 }
 
-renderWaveform();
-
-//play audio after click & pause too
-let isPlaying = false;
 function togglePlayback() {
+  if (!audioContext) {
+    initAudio();
+  }
+
   if (isPlaying) {
     audioElement.pause();
   } else {
     audioElement.play();
   }
-  isPlaying = !isPlaying; 
+
+  isPlaying = !isPlaying;
 }
 
 document.addEventListener('click', togglePlayback);
 
-function handleSongEnd() {
-  togglePlayback();
-}
+audioElement.addEventListener('ended', () => {
+  isPlaying = false;
+});
 
-audioElement.addEventListener('ended', handleSongEnd);
+audioElement.addEventListener('play', () => {
+  requestAnimationFrame(renderWaveform);
+});
+
+document.body.addEventListener('click', () => {
+  if (audioContext && audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
+});
